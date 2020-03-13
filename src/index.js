faf4c996a2d08586887ebd174fd8e2656a2e117e
ganta <sganta.bcj@gmail.com>
2020-03-13 14:42:45 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const calc = require('./calc')
+const _ = require('lodash')
+
+_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 });
+
+const numbersToAdd = [
+  3,
+  4,
+  10,
+  2
+]
+
+const result = calc.sum(numbersToAdd)
+console.log(`The sum of ${numbersToAdd} is: ${result}`)
